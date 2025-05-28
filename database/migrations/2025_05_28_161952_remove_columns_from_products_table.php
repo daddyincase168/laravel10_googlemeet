be3c356cb74d39719086b92db3a17d1fb100d497
daddyincase168 <somonorhong011@gmail.com>
2025-05-28 23:23:30 +0700

    remove column

--- database/migrations/2025_05_28_161952_remove_columns_from_products_table.php
+++ database/migrations/2025_05_28_161952_remove_columns_from_products_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::table('products', function (Blueprint $table) {
             $table->dropColumn('color');
-            $table->dropColumn('size');
+            $table->dropColumn('size');//
         });
     }
 
